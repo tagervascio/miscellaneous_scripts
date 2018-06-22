f94a6dc7e4e94141e9e4241e3205ff46654525e1
tagervascio <tim.gervascio@gmail.com>
2018-06-21 17:47:53 -0700

    automatically generates column numbers w/ headers

--- iterative_cont.pl
+++ iterative_cont.pl
@@ -15,33 +15,19 @@ use Bio::SeqIO;          #Bioperl @ bioperl.org
 
 binmode STDOUT, ":utf8"; #Quiet the "wide character warning" on print()
 
+my $csv_path = "/users/tim/dev/test_csv.csv";
+my $user_email = 'notanactualemail@email.com';
+#print colored(['red on_bright_yellow'],"Databases sometimes require you to send an email address with your request, enter one now: ");
+#chomp(state $user_email=<STDIN>);
 
-#TODO: create the %loci hash automatically using the header row of the csv (first row)
-
-print colored(['red on_bright_yellow'],"Databases sometimes require you to send an email address with your request, enter one now: ");
-chomp(state $user_email=<STDIN>);
-
-#These next three lines create the directory ~/.snailpipe and a subfolder named
-#after the system date and time for each run of the script. The 'eutils'
-#subroutine places its output files in this handy directory to keep things
-#organized.
-my $date_time = localtime =~ s/ /_/gr;
-state $output_path = "$ENV{HOME}/.snailpipe/$date_time";
-mkpath("$output_path") or die "Cannot make directory $output_path: $! \n";
-
-my %loci = (
-        'apples'        =>      1,
-        'oranges'       =>      2,
-        'strawberries'  =>      3,
-        'cranberry'     =>      4,
-);
-my @loci_list = values %loci; #list of column numbers to use e.g. 1,2,3,4,...
-
+my $output_path = &output_path;
+my %loci = loci_hash($csv_path);
+my @loci_list = values %loci;
 
 #This loop extracts the data from the csv one column at a time and passes it to
 #the bioperl eutilitues wrapper in for downloading.
 foreach my $column(@loci_list){
-        my @collected_data = &column_grabber($column);
+        my @collected_data = &column_grabber($column,$csv_path);
         my $loci_name = shift (@collected_data);
         print "$loci_name \n";
         &eutils(\@collected_data, $loci_name);
@@ -51,13 +37,39 @@ print "Check the $output_path directory for your sequences!\n
 HEADS UP: MacOS changes the : to / in the file name.\n
 Happy aligning :)\n";
 
+sub output_path{
+        #Makes the directory ~/.snailpipe with a subfolder for each run of the script.
+        my $date_time = localtime =~ s/ /_/gr;
+        my $output_path = "$ENV{HOME}/.snailpipe/$date_time";
+        mkpath("$output_path") or die "Cannot make directory $output_path: $! \n";
+        return $output_path;
+}
+
+sub loci_hash {
+        #Takes the first row of a csv (the headers) and makes a hash of them with
+        #the value corresponding to the column number. (loci => column).
+        my @file = @_;
+        my $csv = Text::CSV_XS->new ({ binary => 1, eol => $/ });
+        open my $fh, "<", "@file" or die "@file: $!";
+        my $header = $csv->getline ($fh);
+        close $fh or die "@file.csv: $!";
+
+        my $discarded_label = shift @$header;
+
+        my $i = 1;
+        my %loci = map { $_ => $i++} @$header;
+        return %loci;
+}
+
 sub column_grabber {
         #This subroutine accepts a scalar column number and returns the data
         #from the column as @data.
-        my $csv = Text::CSV_XS->new ({ binary => 1, auto_diag => 1 });
-        open my $fh, "<", "/users/tim/dev/test_csv.csv" or
-                die "Something is wrong with your csv: $!";
         my $column = shift (@_);
+        my @csv_path = shift (@_);
+        my $csv = Text::CSV_XS->new ({ binary => 1, auto_diag => 1 });
+        open my $fh, "<", "@csv_path" or
+                die "Something is wrong with your csv: $!";
+
         my @data = map { $_ ->[$column] } @{$csv->getline_all ($fh)};
         return @data;
 
